23ed1894eddf75a814b0b119dca9257d3a4d9421
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2020-12-01 11:50:18 +0530

    Update main.c

--- Stack Lined List/main.c
+++ Stack Lined List/main.c
@@ -1,14 +1,65 @@
-//
-//  main.c
-//  Stack Lined List
-//
-//  Created by Aagman Suri on 01/12/20.
-//
-
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+struct Node
+{
+    int data;
+    struct Node * next;
+}*top =NULL;
+
+void push(int x)
+{
+    struct Node *t;
+    t=(struct Node*)malloc(sizeof(struct Node));
+    if(t==NULL){
+        printf("Stack is full\n");
+    }
+    else{
+    t->data=x;
+    t->next=top;
+    top=t;
+    }
+}
+
+int pop()
+{
+    int x=-1;
+    struct Node *t;
+    
+    if(top==NULL)
+    {
+        printf("Stack is Empty\n");
+    }
+    else
+    {
+        t=top;
+        top=top->next;
+        x=t->data;
+        free(t);
+    }
+    return x;
+    
+}
+void Display()
+{
+    struct Node *p;
+    p=top;
+    while(p)
+    {
+        printf("%d ",p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    push(10);
+    push(40);
+    push(20);
+    push(30);
+    
+    Display();
+    
+    printf("%d ",pop());
 }
