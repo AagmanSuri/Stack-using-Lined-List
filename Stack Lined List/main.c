57893fd3b86bb4eb8ffa8de5a4be3108050603aa
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2020-12-01 11:03:18 +0530

    Initial Commit

---
+++ Stack Lined List/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Stack Lined List
+//
+//  Created by Aagman Suri on 01/12/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
